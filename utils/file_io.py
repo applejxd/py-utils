3a5cf1234f306c766090a1f7faab5e3ad4af3129
applejxd <9639592+applejxd@users.noreply.github.com>
2021-11-16 07:57:28 +0900

    update

--- utils/file_io.py
+++ utils/file_io.py
@@ -7,6 +7,11 @@ import os
 _logger = SelfLogger.get_logger(__file__)
 
 
+def make_dir(dir_name: str) -> None:
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+
+
 def read_csv(file_name: str) -> pd.DataFrame:
     table = dd.read_csv(file_name).compute()
     _logger.info("CSV file read.")
@@ -14,9 +19,7 @@ def read_csv(file_name: str) -> pd.DataFrame:
 
 
 def read_pickle(file_name: str) -> pd.DataFrame:
-    if not os.path.exists("./output"):
-        os.mkdir("./output")
-
+    make_dir("./output")
     with open(f"./output/{file_name}.pkl", "rb") as f:
         result = pickle.load(f)
     _logger.info("Data read from a pickle.")
@@ -24,9 +27,7 @@ def read_pickle(file_name: str) -> pd.DataFrame:
 
 
 def write_pickle(data, file_name: str) -> None:
-    if not os.path.exists("./output"):
-        os.mkdir("./output")
-
+    make_dir("./output")
     with open(f"./output/{file_name}.pkl", "wb") as f:
         pickle.dump(data, f)
     _logger.info("Data saved as a pickle.")
